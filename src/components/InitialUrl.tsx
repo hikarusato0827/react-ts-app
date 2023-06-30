b4711a1838be82dac36f68f47811bb936abafcda
hikaru_sato <hikaru_sato@jsl.co.jp>
2023-06-30 14:38:48 +0900

    コンフリクトするコードを記述

--- src/components/InitialUrl.tsx
+++ src/components/InitialUrl.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import TableHead from '@mui/material/TableHead';
-import { Box, Paper, Table, TableBody, TableRow } from '@mui/material';
+import { Button, Paper, Table, TableBody, TableRow } from '@mui/material';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 
@@ -22,29 +22,28 @@ export const InitialUrl = () => {
     },[]);
 
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                    <TableCell>id</TableCell>
-                    <TableCell>name</TableCell>
-                    <TableCell>username</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody> 
-                    {data?.map((row) => (
-                    <TableRow key={row.id}>
-                        <TableCell>{row.id}</TableCell>
-                        <TableCell>{row.name}</TableCell>
-                        <TableCell>{row.username}</TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-                <button>a</button>
-                <button>b</button>
-                aaaaaaa
-                <p>oooooo</p>
-            </Table>
-        </TableContainer>
+        <div>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                        <TableCell>id</TableCell>
+                        <TableCell>name</TableCell>
+                        <TableCell>username</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody> 
+                        {data?.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell>{row.id}</TableCell>
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.username}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Button>編集</Button>
+        </div>
     );  
     } 
